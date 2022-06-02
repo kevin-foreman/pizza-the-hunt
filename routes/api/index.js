40bbc25ea294e8e7b1ebd19ff3f0ae86b995fda0
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-06-01 19:11:21 -0500

    added some features of the comment model 18.2.6

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
+const commentRoutes = require('./comment-routes');
+
+
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use('/pizzas', pizzaRoutes);
+
+// add prefix of `/comments` to routes created in `comment-roujtes.js`
+router.use('/comments', commentRoutes);
+
+
 
 module.exports = router;
