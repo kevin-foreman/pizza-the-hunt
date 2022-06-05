c67c20583c43479f9e86ff3788cd94e0bdea6a32
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-06-05 17:05:32 -0500

    Update heroku

--- models/Test.js
+++ models/Test.js
@@ -1,1 +1,68 @@
 // A simple test model to try to get heroku to update
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReplySchema = new Schema(  
+
+    {
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        writtenBy: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJson: {
+            getters: true
+        }
+    }
+);
+
+const CommentSchema = new Schema(
+    {
+        writtenBy: {
+            type: String,
+            required: true
+        },
+        commentBody: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => (createdAtVal)
+        },
+        replies: [ReplySchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// get total count of replies with a virtual
+CommentSchema.virtual('replyCount').get(function () {
+    return this.replies.length;
+});
+
+const Comment = model('Comment', CommentSchema);
+// making this change to force heroku deployment
+
+module.exports = Comment;
